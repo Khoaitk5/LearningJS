88d86b0d607f93d13a1b172f4486a6d54bd6386f
khoaitk5 <dangkhoa.itk5@gmail.com>
2024-11-02 10:01:42 +0700

    js
    print and vaiable

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+console.log("Hello");
+window.alert("This is an alert!");
+document.getElementById("myh1").textContent = "this text in js";
+document.getElementById("myp").textContent = "go pizza?";
+*/
+//this is a comment
+
+let x = "Hello there!";
+let y = 18;
+
+console.log(typeof x);
+console.log(`x equals ${x}`);
+
+document.getElementById("myp").textContent = `x = ${x} and y = ${y}`;
